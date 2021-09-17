b6040d7203380ee8753ad1a9e8a91f6ed8b9d5ce
Fabian Finke <finkef@icloud.com>
2021-09-17 17:30:05 +0200

    chore: add next example

---
+++ example/next-example/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to React Native</Text>
+    </View>
+  )
+}
---
+++ example/next-example/next.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// @generated: @expo/next-adapter@2.1.0
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
+const { withExpo } = require('@expo/next-adapter')
+const withPlugins = require('next-compose-plugins')
+
+const withTM = require('next-transpile-modules')([])
+
+module.exports = withPlugins([
+  withTM({
+    webpack: (config) => {
+      return config
+    },
+  }),
+  [
+    withExpo,
+    {
+      projectRoot: __dirname,
+    },
+  ],
+])
