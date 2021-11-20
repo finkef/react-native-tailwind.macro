eb746794dddb52e1c0cf9fcccf6cf7e7a7206d93
Fabian Finke <finkef@icloud.com>
2021-11-20 15:50:12 +0100

    feat: memoize useTailwindStyles wrapper output

--- packages/react-native-tailwind.macro/src/use-tailwind-styles.ts
+++ packages/react-native-tailwind.macro/src/use-tailwind-styles.ts
@@ -1,6 +1,10 @@
+import { useMemo } from "react"
+
 /**
- * Placeholder function
+ * Simple wrapper around the arrow function passed to `useTailwindStyles`. Memoizes the result until
+ * deps (including compiled styles) change.
  */
-export const useTailwindStyles = (_: any, _deps: any) => {
-  return _()
+export const useTailwindStyles = (fn: () => any, deps: any) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(fn, deps)
 }
